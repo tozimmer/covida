4f2a2d3d926c454b2e1d598da42844166eda2267
Markus Weber <markus.weber80@gmail.com>
2013-05-15 09:09:14 +0200

    Adding media description for image.
    Signed-off-by: Markus Weber <markus.weber80@gmail.com>

---
+++ covida-core/src/main/java/de/dfki/covida/covidacore/data/ImageMediaData.java
@@ -0,0 +1,71 @@
+/*
+ * VideoMediaData.java
+ *
+ * Copyright (c) 2012, Tobias Zimmermann All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer. Redistributions in binary form must reproduce the
+ * above copyright notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the author nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package de.dfki.covida.covidacore.data;
+
+import java.io.Serializable;
+import java.util.UUID;
+import javax.xml.bind.annotation.XmlElement;
+
+/**
+ * Video media data
+ *
+ * @author Tobias Zimmermann <Tobias.Zimmermann@dfki.de>
+ */
+public class ImageMediaData implements Serializable {
+
+    /**
+     * serialVersionUID
+     */
+    private static final long serialVersionUID = 5408416424492049777L;
+    /**
+     * Video source as {@link String}
+     */
+    @XmlElement(name = "source")
+    public String imageSource;
+    /**
+     * Video name as {@link String}
+     */
+    @XmlElement(name = "name")
+    public String imageName;
+    /**
+     * Video width in pixel as {@link Integer}
+     */
+    @XmlElement(name = "width")
+    public int width;
+    /**
+     * Video height in pixel as {@link Integer}
+     */
+    @XmlElement(name = "height")
+    public int height;
+
+    /**
+     * UUID which is generated from the application
+     */
+    @XmlElement(name = "uuid")
+    public UUID uuid;
+}
